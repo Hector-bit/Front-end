945d0145cc76390b23ef01f59c42aed355c8ea29
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-23 00:29:26 -0700

    check

--- business-card-organizer/src/utils/axiosWithAuth.js
+++ business-card-organizer/src/utils/axiosWithAuth.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//this axios with auth is what is used to retrieve a token from the server.js
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
   // return an instance of axios
